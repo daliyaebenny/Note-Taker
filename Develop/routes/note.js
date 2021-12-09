fce7908e21e6222bfbc2d825d54ad659743dfad9
Daliya Benny <daliyaebenny@gmail.com>
2021-12-08 22:52:59 -0800

    Modified get,post and delete routes

--- Develop/routes/note.js
+++ Develop/routes/note.js
@@ -1,52 +1,54 @@
 //create route outside server
 const path = require('path');
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend,writeToFile} = require('../helpers/fsUtils');
 const router = require('express').Router();
 const fs = require('fs');
 const uniqid = require('uniqid');
 
 
 
-
+//GET route to get all the notes
 router.get('/api/notes', (req, res) => {
     console.info(`${req.method} request received for notes`);
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('./db/db.json','utf8').then((data) => res.json(JSON.parse(data)));
   });
 
-// router.post('/api/notes',(req,res)=>{
-//   readFromFile('./db/db.json')
-//   .then((data)=>{
-//     const dbNotes = JSON.parse(data);
-//     console.log(dbNotes);
-//     const newNotes = req.body;
-//     const id = 'id';
-//     const noteId = uniqid();
-//     newNotes[id] = noteId;
-//     dbNotes.push(newNotes);
-//     fs.writeFile('db/db.json',JSON.stringify(dbNotes),(err)=>{
-//       if (err) throw err;
-//       res.json(dbNotes);
-//     })
-//   }) 
-// });
+
+//POST route to add a note 
+router.post('/api/notes',(req,res)=>{
+  readFromFile('./db/db.json')
+  .then((data)=>{
+    const dbNotes = JSON.parse(data);
+    console.log(dbNotes);
+    const newNotes = req.body;
+    const id = 'id';
+    const noteId = uniqid();
+    newNotes[id] = noteId;
+    dbNotes.push(newNotes);
+    fs.writeFile('db/db.json',JSON.stringify(dbNotes),(err)=>{
+      if (err) throw err;
+      res.json(dbNotes);
+    })
+  }) 
+});
 
 
-// DELETE Route for a specific note
-// router.delete('api/notes/:id', (req, res) => {
-//   const noteId = req.params.id;
-//   readFromFile('./db/db.json')
-//     .then((data) => JSON.parse(data))
-//     .then((json) => {
-//       // Make a new array of all notes except the one with the ID provided in the URL
-//       const result = json.filter((notes) => notes.id !== noteId);
+//DELETE Route for a specific note
+router.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notes except the one with the ID provided in the URL
+      const result = json.filter((notes) => notes.id !== noteId);
 
-//       // Save that array to the filesystem
-//       writeToFile('./db/db.json', result);
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
 
-//       // Respond to the DELETE request
-//       res.json(`Item ${noteId} has been deleted 🗑️`);
-//     });
-// });
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
+});
 
 
 
--- Develop/server.js
+++ Develop/server.js
@@ -3,8 +3,7 @@ const express = require('express');
 const homeRoutes = require('./routes/homeroutes')
 const notes = require('./routes/note');
 
- // Require the JSON file and assign it to a variable called `termData`
-// const noteData = require('./db/db.json');
+ 
 // Initialize our app variable by setting it to the value of express()
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,9 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
-app.use(homeRoutes);
 app.use(notes);
+app.use(homeRoutes);
+
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
